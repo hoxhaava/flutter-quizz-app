628eaa0e24bc88d6992b74a3b32a1c4a9008b0d7
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-03 16:30:45 +0100

    Edit StartScreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Start Sceen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter Toni',
+            style: TextStyle(
+              color: Color.fromARGB(255, 238, 220, 254),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
   }
 }
