66ec8f3918e19cde5df174cc33a456a840efb2e4
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-03 16:02:35 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
