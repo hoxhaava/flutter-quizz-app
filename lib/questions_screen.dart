4a6abcd377a551d47ec7c8a993be3486b5cf5de3
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-04 15:24:47 +0100

    Refactor QuiestionsScreen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -30,22 +30,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(
-            currentQuestion.answers[0],
-            () {},
-          ),
-          AnswerButton(
-            currentQuestion.answers[1],
-            () {},
-          ),
-          AnswerButton(
-            currentQuestion.answers[2],
-            () {},
-          ),
-          AnswerButton(
-            currentQuestion.answers[3],
-            () {},
-          ),
+          ...currentQuestion.answers.map((item){
+            return AnswerButton(item, () { });
+          })
         ],
       ),
     );
