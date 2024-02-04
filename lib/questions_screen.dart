8e9caa952631095ce3016ef607432c76b69fcce9
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-04 15:51:44 +0100

    Shuffle Answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,22 +18,27 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment:CrossAxisAlignment.stretch ,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((item){
-            return AnswerButton(item, () { });
-          })
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((item){
+              return AnswerButton(item, () { });
+            })
+          ],
+        ),
       ),
     );
   }
