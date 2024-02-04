110e4a0ade986848c5ab6a26ee8d9b5815f76d25
hoxhahava <hoxha.ava.mak@gmail.com>
2024-02-04 15:16:22 +0100

    Access and display data

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -12,7 +12,7 @@ class AnswerButton extends StatelessWidget {
       onPressed: onClick,
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(
-          vertical: 20,
+          vertical: 10,
           horizontal: 40,
         ),
         backgroundColor: const Color.fromARGB(255, 33, 1, 95),
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 
+import 'package:quiz_app/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -13,21 +15,37 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           const SizedBox(
             height: 30,
           ),
-          AnswerButton()
+          AnswerButton(
+            currentQuestion.answers[0],
+            () {},
+          ),
+          AnswerButton(
+            currentQuestion.answers[1],
+            () {},
+          ),
+          AnswerButton(
+            currentQuestion.answers[2],
+            () {},
+          ),
+          AnswerButton(
+            currentQuestion.answers[3],
+            () {},
+          ),
         ],
       ),
     );
